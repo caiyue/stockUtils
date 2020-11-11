1d89eca6f1ba6044c184c650b7cf1f636dbfd5b7
蔡月 <caiyue03@meituan.com>
2020-11-11 20:41:12 +0800

    修改逻辑

--- stockInfo.py
+++ stockInfo.py
@@ -454,7 +454,7 @@ class StockUtils(object):
                         notEmpty = incomeIncreaseByYear != '--' and profileIncreaseByYear != '--' and income != '--' and profit != '--'
                         increaseHight = notEmpty and ((float(incomeIncreaseByYear) >= 25 and float(profileIncreaseByYear) >= 20) or
                                                       (float(incomeIncreaseByYear) >= 30 and float(profit) >= 400000000 and profit * 1.0 / income >= 0.1) or
-                                                      (float(incomeIncreaseByYear) >= 20 and float(profileIncreaseByYear) >= 20 and float(profit) >= 500000000))
+                                                      (float(incomeIncreaseByYear) >= 15 and float(profileIncreaseByYear) >= 15 and float(profit) >= 500000000))
                         if not increaseHight or count == 2:
                             break
                     except Exception,e:
