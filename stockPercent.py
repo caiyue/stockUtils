165d6339a2968089600b07f61ccb983884202fef
蔡月 <caiyue03@meituan.com>
2021-01-10 17:56:29 +0800

    修改逻辑

--- stockPercent.py
+++ stockPercent.py
@@ -377,6 +377,8 @@ def formatStock(arr):
                 isOK = True
             else:
                 isOK = incodeIncremnt >= incomeBaseIncrease and profitIncrment >= profitBaseIncrease
+        elif jll >= 9 and increaseHight and incodeIncremnt >= 30 and profitIncrment >= 30:
+            return True
 
         # 资金聚集筛选条件
         isCollect = (len(je) >= 3 and je[0] > je[1] > je[2]) or \
