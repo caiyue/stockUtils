f91437c9061ab2af27f1d5e0878fb9cee5695534
蔡月 <caiyue03@meituan.com>
2021-01-16 11:05:03 +0800

    完善逻辑

--- stockPercent.py
+++ stockPercent.py
@@ -364,9 +364,10 @@ def formatStock(arr):
                     (not je[0] > je[1] > je[2] or not holdingsCount[0] < holdingsCount[1] < holdingsCount[2]):
                 continue
 
-        #如果不是高速成长的公司，经营现金流差的公司直接过滤掉
+        #如果不是高速成长的公司，经营现金流差的公司，再给一次机会，看当前季度增长是否高，否则直接过滤掉
         if not increaseHight and not cashIncrease:
-            continue
+            if not (incodeIncremnt >= 50 and profitIncrment >= 50):
+                continue
 
         # 筛选财务指标：企业增长不能太差, >= 20 && >= 10,但是茅台，海天不可能增速那么快，所以也需要特殊处理下,或者最近两年高速成长
         isOK = False
